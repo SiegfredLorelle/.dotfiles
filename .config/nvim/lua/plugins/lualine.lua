bec54f1f4f996e262a5eaab1ae6743f8d17274c9
Siegfred Lorelle Mina <siegfredlorelle09@gmail.com>
2025-06-28 16:10:53 +0800

    neovim: create local theme for statusline (lualine)

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,61 +1,167 @@
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function ()
-            require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'onedark',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    always_show_tabline = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-      refresh_time = 16, -- ~60fps
-      events = {
-        'WinEnter',
-        'BufEnter',
-        'BufWritePost',
-        'SessionLoadPost',
-        'FileChangedShellPost',
-        'VimResized',
-        'Filetype',
-        'CursorMoved',
-        'CursorMovedI',
-        'ModeChanged',
-      },
-    }
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
-}
-    end
 
+    config = function ()
+        local colors = {
+            bg = "#1e1e2e",
+            fg = "#cdd6f4",
+            yellow = "#f9e2af",
+            cyan = "#89dceb",
+            darkblue = "#89b4fa",
+            green = "#a6e3a1",
+            orange = "#fab387",
+            violet = "#f5c2e7",
+            magenta = "#cba6f7",
+            blue = "#74c7ec",
+            red = "#f38ba8",
+        }
+        local theme = {
+            normal = {
+                a = { bg = "None", gui = "bold" },
+                b = { bg = "None", gui = "bold" },
+                c = { bg = "None", gui = "bold" },
+                x = { bg = "None", gui = "bold" },
+                y = { bg = "None", gui = "bold" },
+                z = { bg = "None", gui = "bold" },
+            },
+            insert = {
+                a = { bg = "None", gui = "bold" },
+                b = { bg = "None", gui = "bold" },
+                c = { bg = "None", gui = "bold" },
+                x = { bg = "None", gui = "bold" },
+                y = { bg = "None", gui = "bold" },
+                z = { bg = "None", gui = "bold" },
+            },
+            visual = {
+                a = { bg = "None", gui = "bold" },
+                b = { bg = "None", gui = "bold" },
+                c = { bg = "None", gui = "bold" },
+                x = { bg = "None", gui = "bold" },
+                y = { bg = "None", gui = "bold" },
+                z = { bg = "None", gui = "bold" },
+            },
+            replace = {
+                a = { bg = "None", gui = "bold" },
+                b = { bg = "None", gui = "bold" },
+                c = { bg = "None", gui = "bold" },
+                x = { bg = "None", gui = "bold" },
+                y = { bg = "None", gui = "bold" },
+                z = { bg = "None", gui = "bold" },
+            },
+            command = {
+                a = { bg = "None", gui = "bold" },
+                b = { bg = "None", gui = "bold" },
+                c = { bg = "None", gui = "bold" },
+                x = { bg = "None", gui = "bold" },
+                y = { bg = "None", gui = "bold" },
+                z = { bg = "None", gui = "bold" },
+            },
+            inactive = {
+                a = { bg = "None", gui = "bold" },
+                b = { bg = "None", gui = "bold" },
+                c = { bg = "None", gui = "bold" },
+                x = { bg = "None", gui = "bold" },
+                y = { bg = "None", gui = "bold" },
+                z = { bg = "None", gui = "bold" },
+            },
+        }
+
+        local mode_color = {
+            n = colors.red,
+            i = colors.green,
+            v = colors.blue,
+            [""] = colors.blue,
+            V = colors.blue,
+            c = colors.magenta,
+            no = colors.red,
+            s = colors.orange,
+            S = colors.orange,
+            [""] = colors.orange,
+            ic = colors.yellow,
+            R = colors.violet,
+            Rv = colors.violet,
+            cv = colors.red,
+            ce = colors.red,
+            r = colors.cyan,
+            rm = colors.cyan,
+            ["r?"] = colors.cyan,
+            ["!"] = colors.red,
+            t = colors.red,
+        }
+
+        local mode = {
+            "mode",
+            separator = { left = "", right = "" },
+            right_padding = 2,
+            color = function ()
+               return { bg = mode_color[vim.fn.mode()], fg = colors.bg }
+            end
+        }
+        local filename = {
+            "filename",
+            separator = { right = "" },
+            color = { bg = colors.magenta, fg = colors.bg },
+            cond = function()
+                local fname = vim.fn.expand('%:t')
+                return fname ~= '' and fname ~= '[no name]'
+            end,
+            path = 1,                -- 0: Just the filename
+        }
+
+        require('lualine').setup {
+          options = {
+            icons_enabled = true,
+            theme = theme,
+            component_separators = { left = '', right = ' '},
+            section_separators = { left = '', right = ''},
+            disabled_filetypes = {
+              statusline = {},
+              winbar = {},
+            },
+            ignore_focus = {},
+            always_divide_middle = true,
+            always_show_tabline = true,
+            globalstatus = false,
+            refresh = {
+              statusline = 1000,
+              tabline = 1000,
+              winbar = 1000,
+              refresh_time = 16, -- ~60fps
+              events = {
+                'WinEnter',
+                'BufEnter',
+                'BufWritePost',
+                'SessionLoadPost',
+                'FileChangedShellPost',
+                'VimResized',
+                'Filetype',
+                'CursorMoved',
+                'CursorMovedI',
+                'ModeChanged',
+              },
+            }
+          },
+          sections = {
+            lualine_a = { mode },
+            lualine_b = { filename },
+            lualine_c= {  'branch', 'diff', 'diagnostics'},
+            lualine_x = {'encoding', 'fileformat', 'filetype'},
+            lualine_y = {'progress'},
+            lualine_z = {'location'}
+          },
+          inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {'filename'},
+            lualine_x = {'location'},
+            lualine_y = {},
+            lualine_z = {}
+          },
+          tabline = {},
+          winbar = {},
+          inactive_winbar = {},
+          extensions = {}
+        }
+    end
 }
